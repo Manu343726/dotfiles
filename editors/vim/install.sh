18256b9471e5428e76aa6476fbbb669c0889bad0
Manu343726 <Manu343726@gmail.com>
2015-11-29 11:53:57 +0100

    Install vim plugins and build youcompleteme in editors/vim target

--- editors/vim/install.sh
+++ editors/vim/install.sh
@@ -6,6 +6,13 @@
 # Install vim vundle
 dotfiles_install_remote_component GITHUB VundleVim/Vundle.vim .vim/bundle/Vundle.vim
 
+# Install vim plugins
+vim +PluginInstall +qall
+
 # Install config
 dotfiles_install_component .vim $HOME/.vim
 dotfiles_install_component .vimrc $HOME/.vimrc
+
+# Build YouCompleteMe
+python2 ./.vim/bundle/YouCompleteMe/install.py --clang-completer
+
