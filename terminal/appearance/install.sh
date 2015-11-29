7fab365cbeea9fafed7e2fe6047c3e837185833f
Manu343726 <Manu343726@gmail.com>
2015-11-29 20:39:43 +0100

    Merge .Xresources after install

--- terminal/appearance/install.sh
+++ terminal/appearance/install.sh
@@ -2,3 +2,6 @@
 
 dotfiles_install_remote_component GITHUB tlatsas/xcolors xcolors
 dotfiles_install_component .Xresources $HOME/.Xresources
+
+# Update X configuration
+xrdb -merge $HOME/.Xresources
